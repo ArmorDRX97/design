242bedb351791bf3ff166d5ebb4975ad59d8dd42
ArmorDRX97 <armor.d.r.x@gmail.com>
2020-09-30 10:54:37 +0600

    f

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/design/' : '/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/distest/' : '/',
     "transpileDependencies": [
         "vuetify"
     ]
